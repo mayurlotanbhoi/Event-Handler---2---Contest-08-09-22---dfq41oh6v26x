862ca487daed263dfae0893d6c86c54a8506356e
mayur bhoi <mayurbhoi200@gmail.com>
2022-09-08 18:26:01 +0530

    use event bubbling

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,15 @@ const App = () => {
 
   const handleClick = (event) =>{
     // use console.log
+    console.log("Button id is:-" + event.target.id)
 
   }
-
+  
+  // For example if we click button a, it prints 'Button id is:-button-a'
+  // and same for button-b or any other button we add in the app.
   // do not remove the two buttons or change their id
   return (
-    <div id="main">
+    <div id="main" onClick={handleClick}>
       <button id="button-a">Button A</button>
       <button id="button-b">Button B</button>
     </div>
